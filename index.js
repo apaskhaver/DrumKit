45056ce09b09425cb6c6e0c8feb3cdd2f981f027
apaskhaver <alexandra.paskhaver@gmail.com>
2020-12-22 09:08:33 -0500

    Add images and a sound to drums

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("Been clicked");
+    new Audio("sounds/tom-1.mp3").play();
   });
 }
