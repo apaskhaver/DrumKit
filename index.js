ec9b48696a6ab192ed69e99377144d2a1d74f9dc
apaskhaver <alexandra.paskhaver@gmail.com>
2020-12-24 20:09:59 -0500

    Refactor to change style of button pressed without iterating to find it

--- index.js
+++ index.js
@@ -1,60 +1,22 @@
+function changeColor(buttonLetter) {
+  document.querySelector("." + buttonLetter).classList.add("pressed");
+
+  setTimeout(function () {
+    document.querySelector("." + buttonLetter).classList.remove("pressed");
+  }, 100);
+}
+
 document.addEventListener("keydown", function (e) {
   playSound(e.key);
 
-  var index = 0;
-
-  for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    if (document.querySelectorAll(".drum")[i].textContent === e.key) {
-      index = i;
-    }
-  }
-
-  document.querySelectorAll(".drum")[index].style.color = "white";
-
-  setTimeout(() => {document.querySelectorAll(".drum")[index].style.color = "#DA0463";}, 200);
-
+  changeColor(e.key);
 });
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    this.style.color = "white";
-
-    setTimeout(() => {this.style.color = "#DA0463";}, 200);
 
     playSound(this.textContent);
-
-    switch (this.textContent) {
-      case "w":
-        new Audio("sounds/tom-1.mp3").play();
-        break;
-
-      case "a":
-        new Audio("sounds/tom-2.mp3").play();
-        break;
-
-      case "s":
-        new Audio("sounds/tom-3.mp3").play();
-        break;
-
-      case "d":
-        new Audio("sounds/tom-4.mp3").play();
-        break;
-
-      case "j":
-        new Audio("sounds/snare.mp3").play();
-        break;
-
-      case "k":
-        new Audio("sounds/crash.mp3").play();
-        break;
-
-      case "l":
-        new Audio("sounds/kick-bass.mp3").play();
-        break;
-
-      default:
-        console.log(this.textContent);
-    }
+    changeColor(this.textContent);
   });
 }
 
@@ -91,4 +53,6 @@ function playSound(keyClickedOrTapped) {
     default:
       alert(keyClickedOrTapped + " is not valid.");
   }
+
+
 }
